7fade38f03595f903e089b5d0f9bd4622de52026
Symela <sykom21@gmail.com>
2021-02-09 23:46:39 +0200

    more steps

--- app/src/main/java/com/example/flavorsdemo/MainActivity.kt
+++ app/src/main/java/com/example/flavorsdemo/MainActivity.kt
@@ -8,4 +8,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    //TODO: 12. Try adding different image for the imageView in activity_main layout for each flavor
+
+    //TODO: 13. Run the app for every flavor and see the appearance of the app changing every time
 }
